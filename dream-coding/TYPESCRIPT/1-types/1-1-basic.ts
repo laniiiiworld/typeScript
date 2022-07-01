05a22dcc63006c144e26dd8836d7beeddee3c967
lani <laniiiiworld@gmail.com>
2022-07-01 14:35:07 +0900

    기본 타입 마스터하기1

---
+++ dream-coding/TYPESCRIPT/1-types/1-1-basic.ts
@@ -0,0 +1,74 @@
+{
+  //JavaScript
+  //old: var 💩 호이스팅과 여러가지 예상하지 못하는 문제 발생 위험이 있다.
+  var age = 5;
+  age = 1;
+
+  //let es6
+  let name = 'hello';
+  name = 'hi';
+
+  //const
+  const id = '0001';
+}
+
+{
+  /**
+   * JavaScript
+   * Primitive: number, string, boolean, bigint, symbol, null, undefined
+   * Object:function, array, ...
+   */
+
+  //number
+  const num: number = -6;
+
+  //string
+  const str: string = 'hello';
+
+  //boolean
+  const bool: boolean = false;
+
+  //undefined
+  let name: undefined; //💩
+  let age: number | undefined;
+  age = undefined;
+  age = 1;
+  function find(): number | undefined {
+    // return 1;
+    return undefined;
+  }
+
+  //null
+  let person: null; //💩
+  let person2: string | null;
+
+  //unknown : 난 어떤 것을 담을지 잘 모르겠어 💩
+  let notSure: unknown = 0;
+  notSure = 'he';
+  notSure = true;
+
+  //any : 난 어떤것이든 담을 수 있어! 💩
+  let anything: any = 0;
+  anything = 'hello';
+
+  //void : 아무것도 리턴하지 않을 때 사용. 비어있는.
+  function print(): void {
+    console.log('hello');
+    return;
+  }
+  let unusable: void = undefined; //💩
+
+  //never : 함수에서 절대 리턴하지 않는 경우에 명시하기 위해 사용
+  function throwError(message: string): never {
+    //message -> server (log)
+    throw new Error(message);
+    //while(true){}
+  }
+  let neverEnding: never; //💩
+
+  //object
+  let obj: object; //💩
+  function acceptSomeObject(obj: object) {}
+  acceptSomeObject({ name: 'lani' });
+  acceptSomeObject({ animal: 'dog' });
+}
